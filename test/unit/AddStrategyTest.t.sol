034b95dfb1e5b4998cff53556e14fba504cd22e1
Haythem Sellami <17862704+haythemsellami@users.noreply.github.com>
2024-05-09 15:48:01 +0300

    test: fix tests

--- test/unit/AddStrategyTest.t.sol
+++ test/unit/AddStrategyTest.t.sol
@@ -9,18 +9,18 @@ contract AddStrategyTest is FourSixTwoSixAggBase {
     }
 
     function testAddStrategy() public {
-        uint256 allocationPoints = type(uint120).max;
+        uint256 allocationPoints = 500e18;
 
         assertEq(fourSixTwoSixAgg.withdrawalQueueLength(), 0);
 
         _addStrategy(manager, address(eTST), allocationPoints);
 
-        assertEq(fourSixTwoSixAgg.totalAllocationPoints(), allocationPoints);
+        assertEq(fourSixTwoSixAgg.totalAllocationPoints(), allocationPoints + CASH_RESERVE_ALLOCATION_POINTS);
         assertEq(fourSixTwoSixAgg.withdrawalQueueLength(), 1);
     }
 
     function testAddStrategy_FromUnauthorizedAddress() public {
-        uint256 allocationPoints = type(uint120).max;
+        uint256 allocationPoints = 500e18;
 
         assertEq(fourSixTwoSixAgg.withdrawalQueueLength(), 0);
 
@@ -29,7 +29,7 @@ contract AddStrategyTest is FourSixTwoSixAggBase {
     }
 
     function testAddStrategy_WithInvalidAsset() public {
-        uint256 allocationPoints = type(uint120).max;
+        uint256 allocationPoints = 500e18;
 
         assertEq(fourSixTwoSixAgg.withdrawalQueueLength(), 0);
 
@@ -38,13 +38,13 @@ contract AddStrategyTest is FourSixTwoSixAggBase {
     }
 
     function testAddStrategy_AlreadyAddedStrategy() public {
-        uint256 allocationPoints = type(uint120).max;
+        uint256 allocationPoints = 500e18;
 
         assertEq(fourSixTwoSixAgg.withdrawalQueueLength(), 0);
 
         _addStrategy(manager, address(eTST), allocationPoints);
 
-        assertEq(fourSixTwoSixAgg.totalAllocationPoints(), allocationPoints);
+        assertEq(fourSixTwoSixAgg.totalAllocationPoints(), allocationPoints + CASH_RESERVE_ALLOCATION_POINTS);
         assertEq(fourSixTwoSixAgg.withdrawalQueueLength(), 1);
 
         vm.expectRevert();
