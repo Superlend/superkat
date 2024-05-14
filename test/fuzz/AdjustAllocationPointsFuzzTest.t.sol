c46e749a0751e73223c923501acb929f901161c1
Haythem Sellami <17862704+haythemsellami@users.noreply.github.com>
2024-05-14 12:12:59 +0300

    test: fuzzing

---
+++ test/fuzz/AdjustAllocationPointsFuzzTest.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+import {FourSixTwoSixAggBase, FourSixTwoSixAgg} from "../common/FourSixTwoSixAggBase.t.sol";
+
+contract AdjustAllocationsPointsFuzzTest is FourSixTwoSixAggBase {
+    function setUp() public virtual override {
+        super.setUp();
+
+        uint256 initialStrategyAllocationPoints = 500e18;
+        _addStrategy(manager, address(eTST), initialStrategyAllocationPoints);
+    }
+
+    function testFuzzAdjustAllocationPoints(uint256 _newAllocationPoints) public {
+        _newAllocationPoints = bound(_newAllocationPoints, 0, type(uint120).max);
+
+        uint256 strategyAllocationPoints = (fourSixTwoSixAgg.getStrategy(address(eTST))).allocationPoints;
+        uint256 totalAllocationPointsBefore = fourSixTwoSixAgg.totalAllocationPoints();
+        uint256 withdrawalQueueLengthBefore = fourSixTwoSixAgg.withdrawalQueueLength();
+
+        vm.prank(manager);
+        fourSixTwoSixAgg.adjustAllocationPoints(address(eTST), _newAllocationPoints);
+
+        FourSixTwoSixAgg.Strategy memory strategy = fourSixTwoSixAgg.getStrategy(address(eTST));
+
+        if (_newAllocationPoints < strategyAllocationPoints) {
+            assertEq(
+                fourSixTwoSixAgg.totalAllocationPoints(),
+                totalAllocationPointsBefore - (strategyAllocationPoints - _newAllocationPoints)
+            );
+        } else {
+            assertEq(
+                fourSixTwoSixAgg.totalAllocationPoints(),
+                totalAllocationPointsBefore + (_newAllocationPoints - strategyAllocationPoints)
+            );
+        }
+        assertEq(fourSixTwoSixAgg.withdrawalQueueLength(), withdrawalQueueLengthBefore);
+        assertEq(strategy.allocationPoints, _newAllocationPoints);
+    }
+}
