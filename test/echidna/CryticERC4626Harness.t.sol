6ef8f03c80cd3c671bde4bd6ffec958dc165097f
Haythem Sellami <17862704+haythemsellami@users.noreply.github.com>
2024-07-03 18:39:17 +0300

    add echidna properties tests

--- test/echidna/CryticERC4626Harness.t.sol
+++ test/echidna/CryticERC4626Harness.t.sol
@@ -1,21 +1,20 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-// a16z properties tests
-import {ERC4626Test} from "erc4626-tests/ERC4626.test.sol";
+// echidna erc-4626 properties tests
+import {CryticERC4626PropertyTests} from "crytic-properties/ERC4626/ERC4626PropertyTests.sol";
 // contracts
-import {EulerAggregationLayer} from "../src/core/EulerAggregationLayer.sol";
-import {Rebalancer} from "../src/plugin/Rebalancer.sol";
-import {Hooks} from "../src/core/module/Hooks.sol";
-import {Rewards} from "../src/core/module/Rewards.sol";
-import {Fee} from "../src/core/module/Fee.sol";
-import {EulerAggregationLayerFactory} from "../src/core/EulerAggregationLayerFactory.sol";
-import {WithdrawalQueue} from "../src/plugin/WithdrawalQueue.sol";
-import {AllocationPoints} from "../src/core/module/AllocationPoints.sol";
-// mocks
-import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {EulerAggregationLayer} from "../../src/core/EulerAggregationLayer.sol";
+import {Rebalancer} from "../../src/plugin/Rebalancer.sol";
+import {Hooks} from "../../src/core/module/Hooks.sol";
+import {Rewards} from "../../src/core/module/Rewards.sol";
+import {Fee} from "../../src/core/module/Fee.sol";
+import {EulerAggregationLayerFactory} from "../../src/core/EulerAggregationLayerFactory.sol";
+import {WithdrawalQueue} from "../../src/plugin/WithdrawalQueue.sol";
+import {AllocationPoints} from "../../src/core/module/AllocationPoints.sol";
+import {TestERC20Token} from "crytic-properties/ERC4626/util/TestERC20Token.sol";
 
-contract A16zPropertyTests is ERC4626Test {
+contract CryticERC4626Harness is CryticERC4626PropertyTests {
     uint256 public constant CASH_RESERVE_ALLOCATION_POINTS = 1000e18;
 
     // core modules
@@ -30,7 +29,7 @@ contract A16zPropertyTests is ERC4626Test {
     EulerAggregationLayerFactory eulerAggregationLayerFactory;
     EulerAggregationLayer eulerAggregationLayer;
 
-    function setUp() public override {
+    constructor() {
         rewardsImpl = new Rewards();
         hooksImpl = new Hooks();
         feeModuleImpl = new Fee();
@@ -50,16 +49,11 @@ contract A16zPropertyTests is ERC4626Test {
         });
         eulerAggregationLayerFactory = new EulerAggregationLayerFactory(factoryParams);
 
-        _underlying_ = address(new ERC20Mock());
-        _vault_ = eulerAggregationLayerFactory.deployEulerAggregationLayer(
-            _underlying_, "E20M_Agg", "E20M_Agg", CASH_RESERVE_ALLOCATION_POINTS
+        TestERC20Token _asset = new TestERC20Token("Test Token", "TT", 18);
+        address _vault = eulerAggregationLayerFactory.deployEulerAggregationLayer(
+            address(_asset), "TT_Agg", "TT_Agg", CASH_RESERVE_ALLOCATION_POINTS
         );
-        _delta_ = 0;
-        _vaultMayBeEmpty = false;
-        _unlimitedAmount = false;
-    }
 
-    function testToAvoidCoverage() public pure {
-        return;
+        initialize(address(_vault), address(_asset), false);
     }
 }
