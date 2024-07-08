d0d1c9678beeee2b0ff0af07f70b93438990074e
Haythem Sellami <17862704+haythemsellami@users.noreply.github.com>
2024-07-08 15:55:18 +0300

    add slither

--- src/core/lib/ErrorsLib.sol
+++ src/core/lib/ErrorsLib.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 library ErrorsLib {
     error Reentrancy();
-    error ArrayLengthMismatch();
     error InitialAllocationPointsZero();
     error NegativeYield();
     error InactiveStrategy();
