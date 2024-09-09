8004a7ffad2ee22ab5fbe6bce75e02cc92970c50
Haythem Sellami <17862704+haythemsellami@users.noreply.github.com>
2024-09-09 21:58:07 +0700

    Remove max checks on deposit & mint

--- src/lib/ErrorsLib.sol
+++ src/lib/ErrorsLib.sol
@@ -27,10 +27,6 @@ library ErrorsLib {
     error MaxStrategiesExceeded();
 
     /// ERC4626Upgradeable.sol errors
-    /// @dev Attempted to deposit more assets than the max amount for `receiver`.
-    error ERC4626ExceededMaxDeposit(address receiver, uint256 assets, uint256 max);
-    /// @dev Attempted to mint more shares than the max amount for `receiver`.
-    error ERC4626ExceededMaxMint(address receiver, uint256 shares, uint256 max);
     /// @dev Attempted to withdraw more assets than the max amount for `receiver`.
     error ERC4626ExceededMaxWithdraw(address owner, uint256 assets, uint256 max);
     /// @dev Attempted to redeem more shares than the max amount for `receiver`.
